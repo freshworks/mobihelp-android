12f905c7b9e9f4685311c9605c518d5cfeafb429
Prasannan N <prasannan@freshdesk.com>
2014-06-04 21:57:24 +0530

    Adding Custom Theme Demo
    Signed-off-by: Prasannan N <prasannan@freshdesk.com>

---
+++ MobihelpCustomThemeDemo/README.txt
@@ -0,0 +1,9 @@
+Steps to make this Custom Theme Sample work
+
+1. Make sure you have imported the latest version of Mobihelp SDK & reference the same from this sample.
+2. Open Mobihelp SDK's AndroidManifest.xml file and remove all "android:theme" entries. 
+3. Make sure you have a theme setup in your app's AndroidManifest.xml as all the SDK activities will inherit your app's theme
+4. Copy all the style elements from the right variant of Theme.MobihelpSDK to your theme.
+Eg. If your theme inherits from Theme.AppCompat (Dark Theme), copy style elements from Theme.MobihelpSDK
+	If your theme inherits from Theme.AppCompat.Light (Light Theme), copy style elements from Theme.MobihelpSDK.Light
+5. Do a clean build to allow manifest merger to run during build process and launch the application.
---
+++ MobihelpCustomThemeDemo/src/com/example/mobihelp/customthemedemo/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.mobihelp.customthemedemo;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.freshdesk.mobihelp.FeedbackType;
+import com.freshdesk.mobihelp.Mobihelp;
+import com.freshdesk.mobihelp.MobihelpConfig;
+
+/**
+ * Please check the README.txt file included in the root directory of this
+ * project and make the necessary modifications before running this sample.
+ */
+public class MainActivity extends ActionBarActivity {
+
+	public void onCreate(Bundle savedInstance) {
+		super.onCreate(savedInstance);
+		
+		setContentView(R.layout.activity_main);
+		
+		MobihelpConfig config = new MobihelpConfig("https://yourfreshdeskdomain.freshdesk.com",
+				"your-app-id-here", "your-app-secret-here");
+		
+		config.setFeedbackType(FeedbackType.NAME_AND_EMAIL_REQUIRED);
+		config.setLaunchConversationAfterTicketCreation(false);
+		Mobihelp.init(this, config);
+
+		// Add Custom data pertaining to your application 
+		Mobihelp.addCustomData("User Type", "Paid");
+		Mobihelp.addCustomData("Level Completed", "14");
+		Mobihelp.addCustomData("Achievements Unlocked", "Explorer");
+
+		// Drop BreadCrumbs to track user activity
+		Mobihelp.leaveBreadCrumb(this.getLocalClassName());
+		
+		Button btnSupport = (Button) findViewById(R.id.btnSupport);
+		Button btnTalkToUs = (Button) findViewById(R.id.btnTalkToUs);
+		Button btnAppRateDialog = (Button) findViewById(R.id.btnAppRateDialog);
+		
+		btnSupport.setOnClickListener(btnClickListener);
+		btnTalkToUs.setOnClickListener(btnClickListener);
+		btnAppRateDialog.setOnClickListener(btnClickListener);
+	}
+	
+	OnClickListener btnClickListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			switch (v.getId()) {
+			case R.id.btnSupport:
+				// Launch Support 
+				Mobihelp.showSupport(MainActivity.this);
+				break;
+
+			case R.id.btnTalkToUs:
+				// Launch Feedback Directly
+				Mobihelp.showFeedback(MainActivity.this);
+				break;
+				
+			case R.id.btnAppRateDialog:
+				// Manually prompt for an App Rating/Feedback Dialog
+				Mobihelp.showAppRateDialog(MainActivity.this);
+				break;
+			}
+		}
+	};
+}
